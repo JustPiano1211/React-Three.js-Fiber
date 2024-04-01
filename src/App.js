b51182ddcca3a4d6ecea5f1e2d242162a5d0d4b6
JustPiano <topdeveloper97@gmail.com>
2024-04-01 04:56:28 +0300

    add: simple box, and lights

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
 
-function App() {
+const Box = (props) => {
+  const meshRef = useRef();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1.8, 1.8, 1.8]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
 }
+
+const App = () => (
+  <Canvas>
+    <ambientLight intensity={1} />
+    <spotLight position={[10, 10, 10]} angle={0.15} lookAt={[0, 0, 0]} penumbra={1} decay={0} intensity={Math.PI} />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <directionalLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>
+)
 
 export default App;
